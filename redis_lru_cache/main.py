8e746c3b36071e3b0403c849f70e0eb63bacba55
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-08 10:51:42 +0200

    creating some function and testing using of cache

--- redis_lru_cache/main.py
+++ redis_lru_cache/main.py
@@ -1,5 +1,27 @@
 import redis
 from redis_lru import RedisLRU
+import time
 
 client = redis.StrictRedis(host='localhost', port=6379, password=None)
 cache = RedisLRU(client=client, default_ttl=600)
+
+
+@cache
+def some_func(n: int):
+
+    return 1000000 ** n
+
+
+start_1 = time.time()
+some_func(100000)
+finish_1 = time.time()
+without_cache_time = finish_1 - start_1
+
+start_2 = time.time()
+some_func(100000)
+finish_2 = time.time()
+using_cache_time = finish_2 - start_2
+
+
+print(without_cache_time)
+print(using_cache_time)
